59a3f9a93e7647c4a3f14ea2ac9834ab860e65ae
wjames5 <will@tekimaki.com>
2009-10-01 13:45:46 +0000

    fix LGPL license ref from non-existant license.txt file to lgpl url

--- admin/admin_polls.php
+++ admin/admin_polls.php
@@ -1,11 +1,11 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_polls/admin/admin_polls.php,v 1.2 2008/06/25 22:21:20 spiderr Exp $
+// $Header: /cvsroot/bitweaver/_bit_polls/admin/admin_polls.php,v 1.3 2009/10/01 13:45:46 wjames5 Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // Copyright (c) 2006, Stephan Borg <wolff_borg@yahoo.com.au>
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 // Initialization
 require_once( '../../bit_setup_inc.php' );
--- admin/admin_polls_inc.php
+++ admin/admin_polls_inc.php
@@ -1,11 +1,11 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_polls/admin/admin_polls_inc.php,v 1.1 2006/03/08 11:26:54 wolff_borg Exp $
+// $Header: /cvsroot/bitweaver/_bit_polls/admin/admin_polls_inc.php,v 1.2 2009/10/01 13:45:46 wjames5 Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // Copyright (c) 2006, Stephan Borg <wolff_borg@yahoo.com.au>
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 if (isset($_REQUEST["pollprefs"])) {
 	if (isset($_REQUEST["poll_comments_per_page"])) {
 		$gBitSystem->storePreference("poll_comments_per_page", $_REQUEST["poll_comments_per_page"]);
